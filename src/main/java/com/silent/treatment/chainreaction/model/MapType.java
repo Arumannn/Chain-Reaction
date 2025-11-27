1a1fccbc9d698efe91964be8dd25bf7e79fe6fd9
Naufal Alfa <108531196+AlfaNaufal@users.noreply.github.com>
2025-11-27 14:15:31 +0700

    Add New Map

--- src/main/java/com/silent/treatment/chainreaction/model/MapType.java
+++ src/main/java/com/silent/treatment/chainreaction/model/MapType.java
@@ -22,6 +22,41 @@ public enum MapType {
             boolean inHorizontalBar = (y >= 3 && y <= 5);
             return inVerticalBar || inHorizontalBar;
         }
+    },
+
+    DIAMOND("Diamond", 11, 11) {
+        @Override
+        public boolean isValid(int x, int y) {
+            int cx = 5; 
+            int cy = 5; 
+            int radius = 5;
+            boolean baseDiamond = Math.abs(x - cx) + Math.abs(y - cy) <= radius;
+            
+            boolean topTipSupport = (y == 0 && (x == 4 || x == 6));
+            boolean bottomTipSupport = (y == 10 && (x == 4 || x == 6));
+            boolean leftTipSupport = (x == 0 && (y == 4 || y == 6));
+            boolean rightTipSupport = (x == 10 && (y == 4 || y == 6));
+            
+            return baseDiamond || topTipSupport || bottomTipSupport || leftTipSupport || rightTipSupport;
+        }
+    },
+    
+    HOURGLASS("Hourglass", 9, 9) {
+        @Override
+        public boolean isValid(int x, int y) {
+            boolean baseShape;
+            if (y <= 4) {
+                baseShape = x >= y && x <= (8 - y);
+            } else {
+                baseShape = x >= (8 - y) && x <= y;
+            }
+
+            boolean topCornersSupport = (y == 1 && (x == 0 || x == 8));
+            
+            boolean bottomCornersSupport = (y == 7 && (x == 0 || x == 8));
+            
+            return baseShape || topCornersSupport || bottomCornersSupport;
+        }
     };
 
     private final String label;
